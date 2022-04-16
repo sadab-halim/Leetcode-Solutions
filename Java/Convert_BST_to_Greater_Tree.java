0fb9667132a39743ad3f8c17b13219a62726813c
Sadab Halim <sadabhalim7@gmail.com>
2022-04-16 23:50:54 +0530

    updated

---
+++ Java/Convert_BST_to_Greater_Tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root==null){
+            return root;
+        }
+        reverseInorder(root);
+        return root;
+    }
+    private void reverseInorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        reverseInorder(root.right);
+        root.val = root.val + sum;
+        sum = root.val;
+        reverseInorder(root.left);
+        return;
+    }
+}
