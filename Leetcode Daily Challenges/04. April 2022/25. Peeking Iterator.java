9f69ad717dbbd41d8242cccc55e3328c05dd1a00
Sadab Halim <sadabhalim7@gmail.com>
2022-04-25 19:29:18 +0530

    updated

---
+++ Leetcode Daily Challenges/04. April 2022/25. Peeking Iterator.java
@@ -0,0 +1,29 @@
+class PeekingIterator implements Iterator<Integer> {
+    
+    Iterator<Integer> iter = null;
+    Integer next = null;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    iter = iterator;
+        if(hasNext()) next = iter.next();
+	}
+	
+	public Integer peek() {
+        return next;
+	}
+	
+	@Override
+	public Integer next() {
+	    Integer currVal = next;
+        if(iter.hasNext()) 
+            next = iter.next();
+        else 
+            next = null;
+        return currVal;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return next != null || iter.hasNext();
+	}
+}
