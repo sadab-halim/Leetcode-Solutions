a400c9ac77eee8eaa0301e74f584afe047045616
Sadab Halim <sadabhalim7@gmail.com>
2022-04-25 08:04:12 +0530

    updated

--- Leetcode Daily Challenges/04. April 2022/02. Valid Palindrome II.java
+++ Leetcode Daily Challenges/04. April 2022/02. Valid Palindrome II.java
@@ -1,3 +1,19 @@
-public class 02. Valid Palindrome II {
+class Solution {
+    public boolean validPalindrome(String s) {
+        return isPalindrome(s, 0, s.length() - 1, false);
+    }
     
+    public boolean isPalindrome(final String s, int low, int high, final boolean isCharDel){
+        while(low < high){
+            if(s.charAt(low) != s.charAt(high)){
+                if(isCharDel){
+                    return false;
+                }
+                return isPalindrome(s, low + 1, high, true) || isPalindrome(s, low, high - 1, true);
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
 }
