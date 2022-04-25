ff1bb8c0668b4ccc1f8fe0aaff43656ada87836a
Sadab Halim <sadabhalim7@gmail.com>
2022-04-25 08:08:58 +0530

    updated

--- Leetcode Daily Challenges/04. April 2022/14. Search in a Binary Search Tree.java
+++ Leetcode Daily Challenges/04. April 2022/14. Search in a Binary Search Tree.java
@@ -1,3 +1,12 @@
-public class 14. Search in a Binary Search Tree {
-    
+public TreeNode searchBST (TreeNode root, int val) {
+    while (root != null) {
+        if (root.val == val){
+            return root;
+        } else if (root.val > val) {
+            root = root.left;
+        } else if (root.val < val) {
+            root = root.right;
+        }
+    }
+    return root;
 }
