617981ff458209b11d3f438991643c23e57c2c3e
Sadab Halim <sadabhalim7@gmail.com>
2022-05-11 21:27:49 +0530

    intiial commit

---
+++ Leetcode Daily Challenges/05. May 2022/11. Count Sorted Vowel Strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countVowelStrings(int n) {
+        int a,e,i,o,u;
+        a = e = i = o = u = 1;
+        
+        for(int t = 1; t < n; t++){
+            
+            a = a + e + i + o + u;
+            e = e + i + o + u;
+            i = i + o + u;
+            o = o + u;
+            u = u;
+        }
+        return a + e + i + o + u;
+    }
+}
