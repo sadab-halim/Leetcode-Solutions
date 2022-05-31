7f17c3b3038ffa745ba9aadd7db453590955b1d6
Sadab Halim <sadabhalim7@gmail.com>
2022-05-31 23:13:32 +0530

    initial commit

---
+++ Leetcode Daily Challenges/05. May 2022/31. Check if a String Contains All Binary Codes of Size K.java
@@ -0,0 +1,12 @@
+public boolean hasAllCodes(String s, int k) {
+    Set<String> st = new HashSet<>();
+    int l = 0, r = k-1;
+    while(r<s.length()){
+        String ss  = s.substring(l,r+1);
+        st.add(ss);
+        l++;r++;
+    }
+    for(var v  :st)
+        System.out.println(v);
+    return (st.size() == Math.pow(2,k));
+}
