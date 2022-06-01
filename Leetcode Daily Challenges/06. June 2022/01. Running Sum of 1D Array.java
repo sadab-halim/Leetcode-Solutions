96b33f4ad5585cebfd01e625384463e9fbe6e21c
Sadab Halim <sadabhalim7@gmail.com>
2022-06-01 21:32:36 +0530

    initial commit

---
+++ Leetcode Daily Challenges/06. June 2022/01. Running Sum of 1D Array.java
@@ -0,0 +1,8 @@
+public int[] runningSum(int[] nums) {
+    int sum = 0; 
+    for(int i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] + sum;
+        sum = nums[i];
+    }
+    return nums;
+}
